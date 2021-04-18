0dcd1d0b9a0ab65c420fe3c492036f8b70065478
AngarosGamer <66310363+AngarosGamer@users.noreply.github.com>
2021-04-18 21:14:03 +0200

    Test fork (google analytics + Phoenix)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,14 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
